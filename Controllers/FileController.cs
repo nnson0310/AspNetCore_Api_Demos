91feffc291a092f00f3fe3b6fe40015beb0a4255
nnson0310 <nnson0310@gmail.com>
2023-05-18 17:12:02 +0700

    Add api for downloading file

---
+++ Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Diagnostics;
+
+namespace AspCoreWebAPIDemos.Controllers
+{
+    [Route("api/download")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider? _fileExtensionContentTypeProvider;
+
+        public FileController(FileExtensionContentTypeProvider? fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(int fileId)
+        {
+            const string pathToFile = "./sample.pdf";
+
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound("File not found");
+            }
+
+            if (!_fileExtensionContentTypeProvider!.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var allBytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(allBytes, contentType, Path.GetFileName(pathToFile));
+
+        }
+    }
+}
